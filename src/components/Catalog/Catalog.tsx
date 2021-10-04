a596f8a1df13b24e93b8e1d70af5198dfabd1a0f
Daniel Dvorkin <dvorkin212@gmail.com>
2021-10-04 10:44:06 -0400

    made some base pages, added bootstrap and got a navbar with react router installed

---
+++ src/components/Catalog/Catalog.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+
+const Catalog = () => (
+  <div>
+    Catalog Component
+  </div>
+);
+
+export default Catalog;
---
+++ src/components/Features/Features.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+
+const Features = () => (
+  <div>
+    Features Component
+  </div>
+);
+
+export default Features;
